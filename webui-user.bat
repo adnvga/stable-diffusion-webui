9563731b1a62fcfdb4eda530ad6da7badd78013f
adanvga <a01796093@tec.mx>
2026-09-19 01:51:38 -0600

    setuptools constrain
    setuptools==80.10.2

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,11 @@
 @echo off
 
-set PYTHON=
-set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set PYTHON=C:\ai\python310\python
+set GIT=C:\ai\git\bin\git
+
+set COMMANDLINE_ARGS=--models-dir "C:\ai\models" --xformers --no-half-vae --listen
+set COMMANDLINE_ARGS=--models-dir "C:\ai\models" --xformers --no-half-vae
+
+set PIP_BUILD_CONSTRAINT=%~dp0build-constraints.txt
 
 call webui.bat
